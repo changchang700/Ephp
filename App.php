bd73b1713d7a3fee2aed37d885fdc8e12b454e71
changchang700 <741715116@qq.com>
2018-05-09 15:15:12 +0800

    第一次

---
+++ App.php
@@ -0,0 +1,21 @@
+<?php
+namespace Ephp;
+
+class App{
+	/**
+	 * 框架版本
+	 */
+	const version = '1.0';
+
+	public function __construct() {
+		
+	}
+	
+	public static function run(){
+		
+	}
+	
+	public function __destruct() {
+		
+	}
+}
